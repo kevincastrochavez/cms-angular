55e2912c87dff0f63e2a747f26446b1ae9587232
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-05-06 17:38:15 -0600

    displayed messages and add messages to the component messageList

--- src/app/messages/message-edit/message-edit.component.ts
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'app-message-edit',
   templateUrl: './message-edit.component.html',
-  styleUrls: ['./message-edit.component.css']
+  styleUrls: ['./message-edit.component.css'],
 })
 export class MessageEditComponent implements OnInit {
+  @ViewChild('subject') subject!: ElementRef;
+  @ViewChild('msgText') msgText!: ElementRef;
+  @Output() addMessageEvent = new EventEmitter<Message>();
+  currentSender: string = 'Kevin';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSendMessage() {
+    const subjectInput = this.subject.nativeElement.value;
+    const msgTextInput = this.msgText.nativeElement.value;
+
+    const newMsg = new Message(
+      '1',
+      subjectInput,
+      msgTextInput,
+      this.currentSender
+    );
+
+    this.addMessageEvent.emit(newMsg);
+    this.subject.nativeElement.value = '';
+    this.msgText.nativeElement.value = '';
   }
 
+  onClear() {
+    this.subject.nativeElement.value = '';
+    this.msgText.nativeElement.value = '';
+  }
 }
--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'app-message-list',
   templateUrl: './message-list.component.html',
-  styleUrls: ['./message-list.component.css']
+  styleUrls: ['./message-list.component.css'],
 })
 export class MessageListComponent implements OnInit {
+  messages: Message[] = [
+    new Message('2', 'Test', 'This is a test', 'Cindy'),
+    new Message('3', 'Test 2', 'This is a test 2', 'Citlalli'),
+    new Message('3', 'Test 3', 'This is a test 3', 'David'),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onAddMessage(message: Message) {
+    this.messages.push(message);
   }
-
 }
