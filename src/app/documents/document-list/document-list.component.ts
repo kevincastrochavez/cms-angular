979a870f75cebf2fb36a4fcb5788a6e33c6a5be3
Kevin <kevin.castro.chavez0@gmail.com>
2022-05-12 10:14:19 -0600

    finished implementing the documents feature, being able to click one of the documents in the list and display the details

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
   templateUrl: './document-list.component.html',
-  styleUrls: ['./document-list.component.css']
+  styleUrls: ['./document-list.component.css'],
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
-  constructor() { }
+  documents: Document[] = [
+    new Document(
+      '1',
+      'CIT 260 - Object Oriented Programming',
+      'Description for Object Oriented Programming',
+      'opp.com'
+    ),
+    new Document(
+      '1',
+      'WDD 430 - Full Stack Web Development',
+      'Description for Full Stack Web Development',
+      'fswd.com'
+    ),
+    new Document(
+      '1',
+      'CIT 425 - Data Warehousing',
+      'Description for Data Warehousing',
+      'dw.com'
+    ),
+    new Document(
+      '1',
+      'CIT 460 - Enterprise Development',
+      'Description for Enterprise Development',
+      'ed.com'
+    ),
+    new Document(
+      '1',
+      'CIT 495 - Senior Practicum',
+      'Description for Senior Practicum',
+      'sp.com'
+    ),
+  ];
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
   }
-
 }
--- src/app/documents/document.model.ts
+++ src/app/documents/document.model.ts
@@ -1,11 +1,9 @@
-import * as internal from 'stream';
-
 export class Document {
   constructor(
     public id: string,
     public name: string,
     public description: string,
-    public url: string,
-    public children: Document[]
-  ) {}
+    public url: string
+  ) // public children: Document[]
+  {}
 }
