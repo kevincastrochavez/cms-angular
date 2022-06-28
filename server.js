ffffd51576b443b0e6ccaaa074df6d1f710f4d05
Kevin <kevin.castro.chavez0@gmail.com>
2022-06-28 09:55:08 -0600

    installed morgan and cookie-parser packages, created new server for app and created files for different routes

--- server.js
+++ server.js
@@ -1,49 +1,73 @@
-const { EADDRINUSE } = require("constants");
-const http = require("http");
-const debug = require("debug")("node-angular");
+// Get dependencies
+var express = require("express");
+var path = require("path");
+var http = require("http");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-const app = require("./backend/app");
+// import the routing file to handle the default (index) route
+var index = require("./server/routes/app");
+const messageRoutes = require("./server/routes/messages");
+const contactRoutes = require("./server/routes/contacts");
+const documentRoutes = require("./server/routes/documents");
 
-const normalizePort = (val) => {
-  const port = parseInt(val, 10);
+// ... ADD CODE TO IMPORT YOUR ROUTING FILES HERE ...
 
-  if (isNaN(port)) return val;
+var app = express(); // create an instance of express
 
-  if (port > 0) return port;
+// Tell express to use the following parsers for POST data
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.use(cookieParser());
 
-  return false;
-};
+app.use(logger("dev")); // Tell express to use the Morgan logger
 
-const onError = (error) => {
-  if (error.syscall !== "listen") throw error;
+// Add support for CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
 
-  const bind = typeof addr === "string" ? "pipe" + addr : "port" + port;
+// Tell express to use the specified director as the
+// root directory for your web site
+app.use(express.static(path.join(__dirname, "dist/cms")));
 
-  switch (error.code) {
-    case EACCES:
-      console.error(bind + " requires eleveted privileges");
-      process.exit(1);
-      break;
-    case EADDRINUSE:
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
+// Tell express to map the default route ('/') to the index route
+app.use("/", index);
+app.use("/messages", messageRoutes);
+app.use("/contacts", contactRoutes);
+app.use("/documents", documentRoutes);
 
-const onListening = () => {
-  const addr = server.address();
-  const bind = typeof addr === "string" ? "pipe" + addr : "port" + port;
-  debug("Listening on " + bind);
-};
+// ... ADD YOUR CODE TO MAP YOUR URL'S TO ROUTING FILES HERE ...
+app.use((req, res, next) => {
+  res.render("index");
+});
 
-const port = normalizePort(process.env.PORT || 3000);
+// Tell express to map all other non-defined routes back to the index page
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/cms/index.html"));
+});
+
+// Define the port address and tell express to use this port
+const port = process.env.PORT || "3000";
 app.set("port", port);
 
+// Create HTTP server.
 const server = http.createServer(app);
-server.on("error", onError);
-server.on("listening", onListening);
 
-server.listen(port);
+// Tell the server to start listening on the provided port
+server.listen(port, function () {
+  console.log("API running on localhost: " + port);
+});
